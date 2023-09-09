158876cb594e043a18658ab8fd72fc226590ef61
peleg125 <peleg@mischel.biz>
2023-09-10 01:13:44 +0300

    updated about and home

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,12 @@
-
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
+  return (
+    <section className='about'>
+      <div>
+        Please enjoy our All in one app, where you can check your emails, notes
+        and books for free!!
+      </div>
+
+      <h1> Made by Ori and Peleg</h1>
     </section>
+  )
 }
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,9 @@
-
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
+  return (
+    <section className='home'>
+      <h1>Welcome to home page!</h1>
+
+      <div>Click on the links above to navigate to your app of choice.</div>
     </section>
+  )
 }
